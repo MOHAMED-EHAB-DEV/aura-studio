0c1050bb3f068ee9a600ec08092e8cb58c1125cd
Mohammed Ehab <mohamedehabam99@hotmail.com>
2026-07-19 16:35:18 +0300

    commit

--- components/pages/HomeClient.tsx
+++ components/pages/HomeClient.tsx
@@ -2,13 +2,15 @@
 import React from "react";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { ManifestoSection } from "@/components/sections/ManifestoSection";
-import { ServicesBento } from "@/components/sections/ServicesBento";
-import { WorkShowcase } from "@/components/sections/WorkShowcase";
-import { ClientMarquee } from "@/components/sections/ClientMarquee";
-import { Testimonials } from "@/components/sections/Testimonials";
-import { CTABanner } from "@/components/sections/CTABanner";
-import { Footer } from "@/components/layout/Footer";
 import { motion } from "motion/react";
+import dynamic from "next/dynamic";
+
+const ServicesBento = dynamic(() => import("@/components/sections/ServicesBento").then((mod) => mod.ServicesBento));
+const WorkShowcase = dynamic(() => import("@/components/sections/WorkShowcase").then((mod) => mod.WorkShowcase));
+const ClientMarquee = dynamic(() => import("@/components/sections/ClientMarquee").then((mod) => mod.ClientMarquee));
+const Testimonials = dynamic(() => import("@/components/sections/Testimonials").then((mod) => mod.Testimonials));
+const CTABanner = dynamic(() => import("@/components/sections/CTABanner").then((mod) => mod.CTABanner));
+const Footer = dynamic(() => import("@/components/layout/Footer").then((mod) => mod.Footer));
 
 export const HomeClient: React.FC = () => {
   return (
